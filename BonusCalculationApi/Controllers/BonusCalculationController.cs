942838c6fdbc85e12bc63449c2c0965b0cf7835a
will111299 <56645985+will111299@users.noreply.github.com>
2022-07-29 12:24:52 -0400

    Initial commit

---
+++ BonusCalculationApi/Controllers/BonusCalculationController.cs
@@ -0,0 +1,25 @@
+﻿using Banking.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BonusCalculationApi.Controllers;
+
+public class BonusCalculationController : ControllerBase
+{
+    private readonly ICalculateBonusesForAccounts _bonusCalculator;
+
+    public BonusCalculationController(ICalculateBonusesForAccounts bonusCalculator)
+    {
+        _bonusCalculator = bonusCalculator;
+    }
+
+    [HttpGet("/bonuscalculations")]
+    public ActionResult GetBonusFor([FromQuery] decimal balance, [FromQuery] decimal amount)
+    {
+        var bonus = _bonusCalculator.AccountDepositOf(balance, amount);
+        var response = new GetBonusResponse(balance, amount, bonus);
+        return Ok(response);
+    }
+}
+
+
+public record GetBonusResponse(decimal balance, decimal amount, decimal bonusEarned);
